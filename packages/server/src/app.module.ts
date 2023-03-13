9d128e8a75c1d7a7e6230738566f804a2c2396dd
RoRux <rorux84@gmail.com>
2023-03-13 21:28:42 +0300

    feat(server): install typeorm

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-postgres'>('POSTGRES_CONNECTION'),
+        username: config.get<string>('POSTGRES_USERNAME'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DATABASE'),
+        host: config.get<string>('POSTGRES_HOST'),
+        port: config.get<number>('POSTGRES_PORT'),
+        entities: [`${__dirname}dist/**/*.entity{.ts,.js}`],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
     }),
   ],
   controllers: [AppController],
