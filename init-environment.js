e3b2369dfb2ff1a9e1def76a116b61612a8be7b1
RoRux <rorux84@gmail.com>
2023-03-15 18:42:59 +0300

    chore: code improvements

--- init-environment.js
+++ init-environment.js
@@ -1,25 +1,25 @@
 const fs = require("fs");
 
-const prefixClient = "VITE_";
+const clientPrefix = "VITE_";
 
 const addPrefixToEnvVariables = (filePath, prefix) => {
   const buffer = fs.readFileSync(filePath);
-  const stringsArray = buffer.toString().split("\n");
-  const prefixedStringsArray = stringsArray.map((str) =>
+  const envVariablesList = buffer.toString().split("\n");
+  const prefixedEnvVariablesList = envVariablesList.map((str) =>
     str !== "\r" ? `${prefix}${str}` : str
   );
-  return prefixedStringsArray.join("\n");
+  return prefixedEnvVariablesList.join("\n");
 };
 
 fs.copyFileSync(".env.development.sample", ".env.development");
 fs.writeFileSync(
   "packages/client/.env.development",
-  addPrefixToEnvVariables(".env.development.sample", prefixClient)
+  addPrefixToEnvVariables(".env.development.sample", clientPrefix)
 );
 fs.copyFileSync(".env.development.sample", "packages/server/.env.development");
 fs.copyFileSync(".env.production.sample", ".env.production");
 fs.writeFileSync(
   "packages/client/.env.production",
-  addPrefixToEnvVariables(".env.production.sample", "VITE_")
+  addPrefixToEnvVariables(".env.production.sample", clientPrefix)
 );
 fs.copyFileSync(".env.production.sample", "packages/server/.env.production");
--- packages/server/src/users/users.service.ts
+++ packages/server/src/users/users.service.ts
@@ -1,24 +1,24 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserEntity } from './user.entity';
+import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(UserEntity) private readonly usersRepository: Repository<UserEntity>) {}
+  constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
 
-  async createUser(dto: CreateUserDto): Promise<UserEntity> {
+  async createUser(dto: CreateUserDto): Promise<User> {
     const newUser = await this.usersRepository.create(dto);
     await this.usersRepository.save(newUser);
     return newUser;
   }
 
-  async getAllUsers(): Promise<UserEntity[]> {
+  async getAllUsers(): Promise<User[]> {
     return this.usersRepository.find();
   }
 
-  async getUserById(id: number): Promise<UserEntity> {
+  async getUserById(id: number): Promise<User> {
     const user = await this.usersRepository.findOneBy({ id });
 
     if (user) {
