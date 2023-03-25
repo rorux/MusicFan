6d88e72b4674513d06087816b301f14c05e02f2d
RoRux <rorux84@gmail.com>
2023-03-26 00:22:54 +0300

    feat(client): add trial button for theme changing

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,25 @@
-import { useEffect } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router/Router';
 
-function App() {
+const ThemeChangingBtn = (): ReactElement => {
+  const [toggleTheme, setToggleTheme] = useState<'dark' | 'light'>('dark');
+
   useEffect(() => {
-    document.body.setAttribute('data-theme', 'light');
-  }, []);
+    document.body.setAttribute('data-theme', toggleTheme);
+  }, [toggleTheme]);
 
   return (
+    <button className="d-block mx-auto" onClick={() => setToggleTheme(toggleTheme === 'dark' ? 'light' : 'dark')}>
+      Изменить тему
+    </button>
+  );
+};
+
+function App() {
+  return (
     <BrowserRouter>
+      <ThemeChangingBtn />
       <Router />
     </BrowserRouter>
   );
