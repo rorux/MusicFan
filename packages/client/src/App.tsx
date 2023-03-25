114db8c08d85dc17d35e7ec8fbfa224b5e84a120
RoRux <rorux84@gmail.com>
2023-03-25 22:24:42 +0300

    feat(client): add basic colors of themes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router/Router';
 
 function App() {
+  useEffect(() => {
+    document.body.setAttribute('data-theme', 'light');
+  }, []);
+
   return (
     <BrowserRouter>
       <Router />
--- packages/client/src/pages/main-page/component.tsx
+++ packages/client/src/pages/main-page/component.tsx
@@ -1,8 +1,20 @@
 import { ReactElement } from 'react';
 import { cn, kebab } from '../../bem';
 
-const block = cn('main-title');
+const block = cn('main-page');
 
 export const MainPage = (): ReactElement => {
-  return <h1 className={kebab(block())}>Главная страница</h1>;
+  return (
+    <div className="container">
+      <h1>Главная страница</h1>
+      <div className={kebab(block('text-block', ['p-3']))}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non vulputate dolor, ac aliquam velit. Duis
+        faucibus sapien in turpis sodales commodo. Maecenas sit amet consequat massa. Duis sit amet pharetra nisi. Nam
+        blandit vestibulum urna, id finibus sapien vulputate eu. Donec ac pretium magna. Nullam ultrices ligula quis
+        velit ultricies, sit amet tempor elit dictum. Pellentesque habitant morbi tristique senectus et netus et
+        malesuada fames ac turpis egestas. Maecenas dui odio, mattis mollis lacus a, imperdiet finibus orci. Quisque
+        quis auctor nisl.
+      </div>
+    </div>
+  );
 };
