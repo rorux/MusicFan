ba385e21e7ffbea8318404465836a926fb51acbd
RoRux <rorux84@gmail.com>
2023-03-28 15:30:08 +0300

    feat(client\lang-switcher): add flag icon

--- packages/client/src/widgets/header/ui/lang-switcher/component.tsx
+++ packages/client/src/widgets/header/ui/lang-switcher/component.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { cn, kebab } from '@bem';
+import i18n from '../../../../resources/i18n';
+import { mappedFlagIcons } from './mapped-flag-icons';
 
 const block = cn('lang-switcher');
 
 export const LangSwitcher = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    (localStorage.getItem('i18nextLng') ?? 'ru') as keyof typeof mappedFlagIcons,
+  );
+
+  const switchLocale = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    setSelectedLanguage(event.target.value as keyof typeof mappedFlagIcons);
+    i18n.changeLanguage(event.target.value);
+  };
+
   return (
-    <div className={kebab(block())}>
-      <select className={kebab(block('select'))} defaultValue="ru" aria-label="Список языков">
+    <div className={kebab(block(undefined, ['ps-1']))}>
+      {mappedFlagIcons[selectedLanguage]}
+      <select className={kebab(block('select'))} defaultValue={selectedLanguage} onChange={switchLocale}>
         <option value="ru">RU</option>
         <option value="en">EN</option>
       </select>
---
+++ packages/client/src/widgets/header/ui/lang-switcher/mapped-flag-icons.tsx
@@ -0,0 +1,4 @@
+export const mappedFlagIcons = {
+  ru: <span className="fi fi-ru"></span>,
+  en: <span className="fi fi-gb"></span>,
+};
