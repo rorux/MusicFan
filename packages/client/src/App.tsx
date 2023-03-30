27d1b8238ebc1e781453fe68bb2ffb54b6f29198
RoRux <rorux84@gmail.com>
2023-03-30 19:25:15 +0300

    feat(client): add react-toastify

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,31 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer, Flip } from 'react-toastify';
 import { IconContext } from 'react-icons';
 import { iconsStyles } from '@context';
 import { Router } from '@router';
 
 function App() {
   return (
-    <BrowserRouter>
-      <IconContext.Provider value={iconsStyles}>
-        <Router />
-      </IconContext.Provider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <IconContext.Provider value={iconsStyles}>
+          <Router />
+        </IconContext.Provider>
+      </BrowserRouter>
+      <ToastContainer
+        transition={Flip}
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 }
 
