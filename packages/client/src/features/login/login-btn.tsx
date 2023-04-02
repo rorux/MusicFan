e5018cc7158f80b97760b6f2f3f4e72a2ef4e1f2
RoRux <rorux84@gmail.com>
2023-04-02 21:24:38 +0300

    feat(client\features): internationalize login and logout buttons

--- packages/client/src/features/login/login-btn.tsx
+++ packages/client/src/features/login/login-btn.tsx
@@ -1,15 +1,18 @@
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { BsArrowLeftShort } from 'react-icons/bs';
 import { cn, kebab } from '@bem';
-import { Link } from 'react-router-dom';
 import { ROUTES } from '@router';
 
 const block = cn('login-btn');
 
 export const LoginBtn = () => {
+  const { t } = useTranslation();
+
   return (
     <Link to={ROUTES.SIGNIN}>
       <span className={kebab(block(undefined, ['rounded-0']))}>
-        <BsArrowLeftShort />
+        {t('login')} <BsArrowLeftShort />
       </span>
     </Link>
   );
--- packages/client/src/features/logout/logout-btn.tsx
+++ packages/client/src/features/logout/logout-btn.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { BsArrowRightShort } from 'react-icons/bs';
 import { cn, kebab } from '@bem';
 import { useAppDispatch } from '@store';
@@ -6,11 +7,12 @@ import { logout } from '@features/auth';
 const block = cn('logout-btn');
 
 export const LogoutBtn = () => {
+  const { t } = useTranslation();
   const dispatch = useAppDispatch();
 
   return (
     <span className={kebab(block(undefined, ['rounded-0']))} onClick={() => dispatch(logout())}>
-      <BsArrowRightShort />
+      {t('logout')} <BsArrowRightShort />
     </span>
   );
 };
